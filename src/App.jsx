1ea43d69ff7dd4073306b948b1d9dcc6fa0e8689
Kenneth Sonoda <114664532+Kenny-still12@users.noreply.github.com>
2026-09-18 19:59:03 +1000

    Project initialization

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <div className="bg-slate-900 p-8 h-screen text-white">
+        <h2 className="text-5xl text-center">Hello World</h2>
+      </div>
+    </>
+  )
+}
